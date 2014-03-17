843c34d9324846c5221e71c1b9dd19fe8ca11b98
tony311 <tony@tonyfox.ws>
2014-03-17 12:33:44 -0400

    Added italian localization stuff

--- localization.lua
+++ localization.lua
@@ -193,6 +193,65 @@ elseif LOCALE == "frFR" then -- French
 	--L["Different"]					= ""
 	--L["Always"]						= ""
 
+elseif LOCALE == "itIT" then -- Italian
+	--L["%s is using an outdated version of WatchXP! Tell them to update it or it won't work with you. Type |cffff7f00/wxp updatewarning|r to disable these messages."]= ""
+	--L["Got XP response from %s (%s). This blip will not automatically update. To remove the blip, type /wxp clear."]= ""
+	
+	--L["Debug output %s"]			= ""
+	--L["enabled"]					= ""
+	--L["disabled"]					= ""
+	--L["Update warning %s"]			= ""
+	--L["Version: %s (%s)"]			= ""
+	
+	--L["Show the options panel"]		= ""
+	--L["Request a marker for a player not in your group (they still need WatchXP)"]= ""
+	--L["Request a marker for a Battle.net friend (they still need WatchXP)"]= ""
+	--L["Request a marker for the nth online Battle.net friend in your friends list (they still need WatchXP)"]= ""
+	--L["Toggle visibility of WatchXP"]= ""
+	--L["Refreshes the display and forces an update from all party members. Also removes leftover markers or markers added with /wxp ask"]= ""
+	--L["Toggle debug output"]		= ""
+	--L["Toggle warning when other players are using an older, incompatible version"]= ""
+	--L["Display addon version"]		= ""
+	
+	--L["Hiding WatchXP"]				= ""
+	--L["Showing WatchXP"]			= ""
+	--L["Sending XP request to %s"]	= ""
+	--L["You must be connected to Battle.net to do that"]= ""
+	--L["Sending XP request to %s"]	= ""
+	
+	--L["Label font size"]			= ""
+	--L["Blip vertical offset"]		= ""
+	--L["Label vertical offset"]		= ""
+	--L["Blip size"]					= ""
+	
+	--L["Level %i"]					= ""
+	--L["XP: %s / %s (%s%%)"]			= ""
+	--L["Labels"]						= ""
+	
+	--L["Example!"]					= ""
+	--L["RealmName"]					= ""
+	
+	--L["Shows markers on the experience bar for party members"] = ""
+	--L["Blip Size"]					= ""
+	--L["Vertical Offset"]			= ""
+	--L["Blip Texture"]				= ""
+	--L["Show WatchXP"]				= ""
+	--L["Animate Blips"]				= ""
+	--L["Label Settings"]				= ""
+	
+	--L["Control display of labels next to markers"]= ""
+	--L["Label Color"]				= ""
+	--L["Label Font"]					= ""
+	--L["Font Size"]					= ""
+	--L["Vertical Offset"]			= ""
+	--L["Show Level in Label"]		= ""
+	--L["Show Realm in Label"]		= ""
+	--L["Show Labels"]				= ""
+	
+	--L["Never"]						= ""
+	--L["Different"]					= ""
+	--L["Always"]						= ""
+
 elseif LOCALE == "ptBR" then -- Brazilian Portugese
 	--L["%s is using an outdated version of WatchXP! Tell them to update it or it won't work with you. Type |cffff7f00/wxp updatewarning|r to disable these messages."]= ""
 	--L["Got XP response from %s (%s). This blip will not automatically update. To remove the blip, type /wxp clear."]= ""
